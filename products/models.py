f3893212a6ef12ff4f9aae98436a7d38e67aac15
Kay Alvaer <62592167+Kayalvaer@users.noreply.github.com>
2021-04-25 00:35:09 +0200

    Customize admin

--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ class Category(models.Model):
     name = models.CharField(max_length=254)
     slug = models.CharField(max_length=254, null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
 
