22fe59e5c953ae8cd883a049472d9e280f90ef9c
Kay Alvaer <62592167+Kayalvaer@users.noreply.github.com>
2021-04-29 21:43:12 +0200

    Add forms file,signals and admin content

---
+++ checkout/signals.py
@@ -0,0 +1,22 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def reload_on_save(sender, instance, created, **kwargs):
+    """
+    Updates total amount on each addition of an 
+    item to the order
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def reload_on_del(sender, instance, **kwargs):
+    """
+    Updates total amount on each deletion 
+    from the order
+    """
+    instance.order.update_total()
