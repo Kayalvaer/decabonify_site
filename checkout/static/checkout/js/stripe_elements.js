dd9c570e082af825e92e32693320a0337ee6394b
Kay Alvaer <62592167+Kayalvaer@users.noreply.github.com>
2021-05-04 23:42:12 +0200

    Fixed correction of misplaced file path and added stripe elements

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -10,7 +10,6 @@ var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
-
 var style = {
     base: {
         color: '#000',
@@ -28,5 +27,5 @@ var style = {
 };
 
 var card = elements.create('card', {style: style});
-card.mount('#card-element');
+card.mount('#card-content');
 
