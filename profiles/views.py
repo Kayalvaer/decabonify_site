31e79c783a197e75512aa4cb662eaad2f1a17342
Kay Alvaer <62592167+Kayalvaer@users.noreply.github.com>
2021-05-09 15:27:54 +0200

    Addition of profile page templates and views and form elements

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ To display client profile """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == "POST":
+        form = serProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Successfully update Profile!")
+        else:
+            messages.error(request, "Something is wrong with your form!")
+    else:
+        form = UserProfileForm(instance=profile)
+
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = "profiles/profile.html"
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
     return render(request, template, context)
