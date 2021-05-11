fec5a84e9808d1f8ab3cc2519753f5418a1bace7
Kay Alvaer <62592167+Kayalvaer@users.noreply.github.com>
2021-05-11 15:27:58 +0200

    updated aws keys and static files

--- decabonify_platform/settings.py
+++ decabonify_platform/settings.py
@@ -193,8 +193,8 @@ if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'decarbonify-site'
     AWS_S3_REGION_NAME = 'eu-north-1'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID') or "AKIAYBUOWPBKXWY2E35L"
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') or "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"
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
